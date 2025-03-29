ddda86bbb8141b9dbbadb3f53fbe05a88a79f36f
ILYASSMOUMNI <moumniilyas6@gmail.com>
2025-03-29 14:27:16 +0000

    ghi root f psswrd

---
+++ settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'your_database_name',
+        'USER': 'root',
+        'PASSWORD': 'your_mysql_password',  # Add your actual MySQL root password here
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+} 
